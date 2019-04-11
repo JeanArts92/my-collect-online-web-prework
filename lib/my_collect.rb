65160081712aa8f2cdd074c3c925631fbda8d0b3
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-04-11 17:20:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,6 @@
-def my_collect(languages, students)
-  i = 0 
-  while i < languesges.length || students.length
-yield(languages[i] || students[i])
-i += 1 
-end
-i 
-end
+def my_collect(array)
+ i = 0 
+ everything = []
+ while i < array.length 
+ 
 
